52967d1294efdee536d17b4a3c46eaf6bce82ab1
Benson-Akin <akinolabenson1027@gmail.com>
2026-06-22 16:15:54 +0100

    remove unnecessary function

--- script.js
+++ script.js
@@ -39,17 +39,12 @@ const init = function () {
 
 init();
 
-const newFeature = function () {
-  console.log('Welcome to the Dices Game');
-};
-
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
   currentScore = 0;
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
-  newFeature();
 };
 
 //Rolling Dice Functionality
